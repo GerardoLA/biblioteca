17ccbc6df2d408a02640fab2393a3dbe52f953a4
Gerardo Larrinaga <Gerardo Larrinaga@HZ321162.H012108.NET>
2023-01-26 12:53:29 +0100

    funciones

--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -1,5 +1,36 @@
 package biblioteca;
 
-public class Menu {
 
+
+public class Menu {
+	
+	
+	public static void menuPrincipal() {
+		System.out.println("Menu Principal");
+		System.out.println("1.- Gestionar Libros");
+		System.out.println("2.- Gestionar Socios");
+		System.out.println("3.- Gestionar Prestamos");
+	}
+	
+	public static void menuLibros() {
+		System.out.println("Menu libros");
+		System.out.println("1.- Insertar Libros");
+		System.out.println("2.- Eliminar Socios");
+		System.out.println("3.- Ver libro");
+	}
+		
+	public static void Menusocio(){
+	System.out.println("Ver socios");	
+	}
+
+	
+	public static void  MenuPrestamo() {
+		System.out.println("Menu prestamos");
+			System.out.println(" 1.- Realizar préstamo");
+			;
+		}
 }
+		
+	
+	
+
