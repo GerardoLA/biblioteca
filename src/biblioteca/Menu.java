7294c799100e2efc56a1fd8e1e863a94e2fba7ef
Gerardo Larrinaga <Gerardo Larrinaga@HZ321162.H012108.NET>
2023-01-30 12:48:03 +0100

    menu

--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -5,26 +5,26 @@ package biblioteca;
 public class Menu {
 	
 	
-	public static void menuPrincipal() {
+	public static void mostrarmenuPrincipal() {
 		System.out.println("Menu Principal");
 		System.out.println("1.- Gestionar Libros");
 		System.out.println("2.- Gestionar Socios");
 		System.out.println("3.- Gestionar Prestamos");
 	}
 	
-	public static void menuLibros() {
+	public static void mostrarmenuLibros() {
 		System.out.println("Menu libros");
 		System.out.println("1.- Insertar Libros");
 		System.out.println("2.- Eliminar Socios");
 		System.out.println("3.- Ver libro");
 	}
 		
-	public static void Menusocio(){
+	public static void mostrarMenusocio(){
 	System.out.println("Ver socios");	
 	}
 
 	
-	public static void  MenuPrestamo() {
+	public static void  mostrarMenuPrestamo() {
 		System.out.println("Menu préstamos");
 			System.out.println(" 1.- Realizar préstamo");
 			;
