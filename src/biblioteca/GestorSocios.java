cdbde5429bb1b5300531993d5e11fec4f2e907ec
Usuario <Usuario@DESKTOP-AB3UK2C>
2023-01-31 21:21:18 +0100

    gestorSocios

--- src/biblioteca/GestorSocios.java
+++ src/biblioteca/GestorSocios.java
@@ -1,7 +1,29 @@
 package biblioteca;
 
+import java.util.Scanner;
+
 public class GestorSocios {
-	public static void run() {
+	public static void run(Scanner scan) {
+		int opcion;
+		do {
+			Menu.mostrarMenusocio();
+			opcion = Integer.parseInt(scan.nextLine());
+			
+			switch (opcion) {
+			case Menu.INSERTAR_SOCIO:
+				break;
+			case Menu.MODIFICAR_SOCIO:
+				break;
+			case Menu.VER_SOCIO:
+				break;
+			case Menu.ELIMINAR_SOCIO:
+				break;
+			case Menu.SALIR:
+				break;
+			}
+			
+			
+		}while(opcion!=Menu.SALIR);
 		
 	}
 
