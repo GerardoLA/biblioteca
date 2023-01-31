72588905ec9648546566f6b166bb0aad2e363c21
Gerardo Larrinaga <Gerardo Larrinaga@HZ321162.H012108.NET>
2023-01-31 09:50:23 +0100

    Menu

--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -5,6 +5,24 @@ package biblioteca;
 public class Menu {
 	
 	
+	public static final int GESTIONAR_LIBROS = 1;
+	public static final int GESTIONAR_SOCIOS = 2;
+	public static final int GESTIONAR_PRESTAMOS = 3;
+	
+	public static final int SALIR = 0;
+	
+	public static final int INSERTAR_LIBRO = 1;
+	public static final int ELIMINAR_LIBRO = 2;
+	public static final int MODIFICAR_LIBRO = 3;
+	public static final int VER_LIBRO = 4 ;
+	
+	public static final int INSERTAR_SOCIO = 1;
+	public static final int ELIMINAR_SOCIO = 2;
+	public static final int MODIFICAR_SOCIO = 3;
+	public static final int VER_SOCIO = 4 ;
+	
+	
+	
 	public static void mostrarmenuPrincipal() {
 		System.out.println("Menu Principal");
 		System.out.println("1.- Gestionar Libros");
