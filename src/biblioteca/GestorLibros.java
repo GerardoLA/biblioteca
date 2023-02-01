730496322d8123811173f2a5e66500796310be15
Usuario <Usuario@DESKTOP-AB3UK2C>
2023-02-01 21:35:58 +0100

    ...

--- src/biblioteca/GestorLibros.java
+++ src/biblioteca/GestorLibros.java
@@ -5,43 +5,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GestorLibros {
-	
 	public static void run(Scanner scan) throws SQLException {
+		
 		int opcion;
 		Libro libro;
 		GestorBBDD gestor = new GestorBBDD();
+		
 		do {
+			
 			Menu.mostrarmenuLibros();
 			opcion = Integer.parseInt(scan.nextLine());
-			
 			switch (opcion) {
-			case Menu.INSERTAR_LIBRO:
-				libro= FormularioDeDatos.pediDatosLibro(scan);
-				gestor.insertarLibro(libro);
-				System.out.println("Libro insertado");
-				break;
+				case Menu.INSERTAR_LIBRO:
+					libro= FormularioDeDatos.pediDatosLibro(scan);
+					gestor.insertarLibro(libro);
+					System.out.println("Libro insertado");
+					break;
 				
-			case Menu.MODIFICAR_LIBRO:
-				int idLibro = FormularioDeDatos.pedirIdLibro(scan);
-				libro = gestor.getLibro(idLibro);
-				Visor.mostrarLibro(libro);
-				FormularioDeDatos.modificarDatos(libro, scan);
-				gestor.modificarLibro(libro);
-				System.out.println("Libro modificado");
-				
-				break;
-			case Menu.VER_LIBROS:
-				
-				ArrayList<Libro>libros = new ArrayList<Libro>();
-				Visor.mostrarLibros(gestor.getLibros());
-				
-				break;
-			case Menu.ELIMINAR_LIBRO:
-				gestor.eliminarLibro(FormularioDeDatos.pedirIdLibro(scan));
-				System.out.println("Libro eliminado");
-				break;
-			case Menu.SALIR:
-				break;
+				case Menu.MODIFICAR_LIBRO:
+					int idLibro = FormularioDeDatos.pedirIdLibro(scan);
+					libro = gestor.getLibro(idLibro);
+					Visor.mostrarLibro(libro);
+					FormularioDeDatos.modificarDatos(libro, scan);
+					gestor.modificarLibro(libro);
+					System.out.println("Libro modificado");
+					
+					break;
+				case Menu.VER_LIBROS:
+					
+					ArrayList<Libro>libros = new ArrayList<Libro>();
+					Visor.mostrarLibros(gestor.getLibros());
+					
+					break;
+				case Menu.ELIMINAR_LIBRO:
+					gestor.eliminarLibro(FormularioDeDatos.pedirIdLibro(scan));
+					System.out.println("Libro eliminado");
+					break;
+				case Menu.SALIR:
+					break;
 			}
 			
 			
